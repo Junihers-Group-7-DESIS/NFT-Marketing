6c001fe2d24399c7a6cd76a547559b1e647c95eb
Eva Sharma <20bcs043@nith.ac.in>
2022-02-04 20:36:14 +0530

    configured

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://public-rpc.blockpi.io/http/polygon-mumbai-testnet",
+      
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: "https://public-rpc.blockpi.io/http/polygon",
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
