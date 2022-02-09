1f60910038d5243d3e61389cf8fb204e0235f716
Eva Sharma <20bcs043@nith.ac.in>
2022-02-09 19:29:33 +0530

    Completed the market contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -121,4 +121,80 @@ contract NFTMarket is ReentrancyGuard {
         payable(owner).transfer(listingPrice);
     }
 
+    //functions that returns all the items that haven't been purchased by anyone
+
+    function fetchMarketItems()public view returns(MarketItem[] memory){
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[] (unsoldItemCount);
+
+        for(uint i=0;i<itemCount;i++){
+            if(idToMarketItem[i+1].owner == address(0)){
+                uint currentId = idToMarketItem[i+1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+
+    //functions that returns all the items the person has purchased
+    function fetchMyNFTs() public view returns (MarketItem[] memory){
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].owner == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].owner == msg.sender){
+                uint currentId = idToMarketItem[i+1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex+=1;
+            }
+        }
+
+        return items;
+    }
+
+    //function that returns all the items that the user has created themselves
+
+    function fetchItemsCreated() public view returns (MarketItem[] memory){
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+
+        for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].seller == msg.sender){
+                itemCount++;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+    
+        for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].seller == msg.sender){
+                uint currentId = idToMarketItem[i+1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex +=1 ;
+            }
+        }
+
+        return items;
+    }
+
+
 }
