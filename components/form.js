a2a7c8e851c77e8da0844ef7b549c5a01bacc21d
sandeepmv <sunny.venki@gmail.com>
2022-03-24 06:42:18 +0530

    Removing sale for creating an nft

--- components/form.js
+++ components/form.js
@@ -68,38 +68,25 @@ const Form = (props) => {
           const url = `https://ipfs.infura.io/ipfs/${added.path}`
       
           //after the file is uploaded on ipfs, pass the url to save it on polygon
-          createSale(url)
+          createMktItem(url)
         }catch(e){
           console.log('Error uploading file: e', e);
         }
     }
 
-    async function createSale(url){
+    async function createMktItem(url){
         const web3Modal = new Web3Modal();
         const connection = await web3Modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
         const signer = provider.getSigner()
-  
-  
-        let contract = new ethers.Contract(nftaddress,NFT.abi,signer)
-        let transaction = await contract.createToken(url)
-  
-        let tx = await transaction.wait()
-        console.log(tx)
-        let event = tx.events[0]
-        let value = event.args[2]
-        let tokenId = value.toNumber()
-  
-  
+    
         const price = ethers.utils.parseUnits(formInput.price, 'ether')
-  
         //move the reference of the contract from the nft contract to the market contract
         contract = new ethers.Contract(nftmarketaddress,Market.abi,signer)
   
         let listingPrice = await contract.getListingPrice()
         listingPrice = listingPrice.toString()
-  
-        transaction = await contract.createMarketItem(nftaddress,tokenId,price, {value: listingPrice})
+        transaction = await contract.createToken(url, price, { value: listingPrice })
   
         transaction.wait()
   
